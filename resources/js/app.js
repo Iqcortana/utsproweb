c24f81acaae46b0d9f5e62081cd582ee8b543dc9
IqCortana <faiq.id2017@gmail.com>
2024-03-14 23:17:15 +0800

    template sudah jalan

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
 // import './bootstrap';
+// import "../js/jquery-min.js";
+// import "../js/popper.min.js";
+// import "../js/bootstrap.min.js";
+// import "../js/owl.carousel.min.js";
+// import "../js/wow.js";
+// import "../js/jquery.nav.js";
+// import "../js/scrolling-nav.js";
+// import "../js/jquery.easing.min.js";
+// import "../js/jquery.counterup.min.js";  
+// import "../js/waypoints.min.js";   
+// import "../js/jquery.slicknav.js";
+// import "../js/main.js";
--- webpack.mix.cjs
+++ webpack.mix.cjs
@@ -17,4 +17,31 @@ mix.js('resources/js/app.js', 'public/js')
             }
          ]
       }
-   });
+   })
+   .styles([
+      "resources/css/bootstrap.min.css" ,
+      "resources/css/slicknav.css",
+      "resources/css/owl.carousel.min.css",
+      "resources/css/owl.theme.css",
+      "resources/css/magnific-popup.css",
+      "resources/css/nivo-lightbox.css",
+      "resources/css/animate.css",
+      "resources/css/main.css",
+      "resources/css/responsive.css"
+   ],
+   "public/css/app.css")
+   .scripts([
+      "resources/js/jquery-min.js",
+      "resources/js/popper.min.js",
+      "resources/js/bootstrap.min.js",
+      "resources/js/owl.carousel.min.js",
+      "resources/js/wow.js",
+      "resources/js/jquery.nav.js",
+      "resources/js/scrolling-nav.js",
+      "resources/js/jquery.easing.min.js",
+      "resources/js/jquery.counterup.min.js",  
+      "resources/js/waypoints.min.js",   
+      "resources/js/jquery.slicknav.js",
+      "resources/js/main.js"
+   ],
+   "public/js/app.js")
